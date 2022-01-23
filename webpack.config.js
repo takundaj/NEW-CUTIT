8d124b774b6727713aea6a33d810f1be89f1514f
Takunda Nyamatore <tjnyamatore@gmail.com>
2022-01-23 23:31:09 +0000

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ let path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "../src/index.js",
+  entry: "/src/index.js",
   output: {
     path: path.resolve(__dirname, "dist/assets"),
     filename: "bundle.js",

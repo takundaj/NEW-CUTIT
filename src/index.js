0d8a72d3846bb6994f7a6eb9d4574fe3e85d7055
Takunda Nyamatore <tjnyamatore@gmail.com>
2022-01-24 09:27:26 +0000

    import "./images"

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "./styles/subscribe-floater.css";
 import "./styles/cookie-consent.css";
 
 // import Swiper and modules styles
+import "./images";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";

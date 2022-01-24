939cd2d1a8e1244e09b2cf2d168e147b4f81dacf
Takunda Nyamatore <tjnyamatore@gmail.com>
2022-01-24 10:38:06 +0000

    installed file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-        use: "html-loader",
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
       },
     ],
   },
